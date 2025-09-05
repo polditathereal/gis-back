84a27d6f151899efa65111dfbf70f85db5bdf2ac
rodrigocarmona1998 <rodrigo.carmona@thoughtworks.com>
2025-09-05 19:24:28 +0200

    l

--- server.js
+++ server.js
@@ -28,6 +28,19 @@ app.use(cors({
   optionsSuccessStatus: 204
 }));
 
+// Añade este middleware para exponer el header en todas las respuestas
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Vary', 'Origin');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  }
+  next();
+});
+
 // Manejo explícito de preflight para todas las rutas
 app.options('*', cors({
   origin: function(origin, callback) {
