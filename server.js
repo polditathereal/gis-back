6034da014fdaaa7bd23a61300b1eac17b83e7c50
rodrigocarmona1998 <rodrigo.carmona@thoughtworks.com>
2025-10-01 00:22:13 +0200

    a

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
