688949e1d91fa44af2c571304334a09fdac0934c
rodrigocarmona1998 <rodrigo.carmona@thoughtworks.com>
2025-10-01 00:18:06 +0200

    a

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const projectsApi = require('./projectsApi');
 const newsApi = require('./newsApi');
